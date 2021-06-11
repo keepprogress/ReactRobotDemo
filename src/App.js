587d4b914d8752206f691a4c7f096f615936c599
Nick.Syu <nick.syu@varmeego.com>
2021-06-11 11:00:10 +0800

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component.jsx';
 import { SearchBox } from './components/search-box/search-box.component';
 
+import './App.css'
+
 class App extends Component {
   constructor() {
     super();
@@ -11,12 +13,19 @@ class App extends Component {
       monsters: [],
       searchField: '',
     };
+
+    // this.handleChange = this.handleChange.bind(this);
+    // use arrow function to write method in class so we wouldn't need to use this bind method
   }
 
   componentDidMount() {
     fetch('http://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState( { monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
   }
 
   render() {
@@ -28,9 +37,10 @@ class App extends Component {
       <div className='App'>
 {/* this.setState is asynchronous function so it will active after console.log(this.state) 
     we can write second argument funtion to run after setState*/}
+      <h1> Monster Rolodex </h1>
       <SearchBox
         placeholder = 'search monster'
-        handleChange = {e => {this.setState({ searchField: e.target.value }, () => console.log(this.state))}}
+        handleChange = {this.handleChange}
       />
       <CardList monsters={filteredMonsters} />
       </div>
